76907a1a297ec7c4c468016b40312f3c4cc10573
Christopher Garza <christopher.pgarza@gmail.com>
2026-03-04 19:06:11 -0600

    feat: Embedded checkout with standard script implementation

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -11,8 +11,8 @@ export default function StorePage() {
     const script = document.createElement('script');
     script.id = 'fsc-api';
     script.src = 'https://sbl.onfastspring.com/sbl/1.0.6/fastspring-builder.min.js';
+    script.type = 'text/javascript';
     script.setAttribute('data-storefront', 'fastspringpoc.test.onfastspring.com/embedded-store-1');
-    script.async = true;
     document.head.appendChild(script);
   }, []);
 
