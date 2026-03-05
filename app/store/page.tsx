d11f8693d8b0e6e38abcfc13b651426b6ae1e5a9
Christopher Garza <christopher.pgarza@gmail.com>
2026-03-04 19:01:20 -0600

    fix: Use correct embedded storefront ID (embedded-store-1)

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -11,7 +11,7 @@ export default function StorePage() {
     const script = document.createElement('script');
     script.id = 'fsc-api';
     script.src = 'https://sbl.onfastspring.com/sbl/1.0.6/fastspring-builder.min.js';
-    script.setAttribute('data-storefront', 'fastspringpoc.test.onfastspring.com/store-1');
+    script.setAttribute('data-storefront', 'fastspringpoc.test.onfastspring.com/embedded-store-1');
     script.async = true;
     document.head.appendChild(script);
   }, []);
